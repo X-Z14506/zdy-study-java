ae99542136beede7f7555cfd8984a0fd87fb42ef
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-10-23 19:32:41 +0800

    Create Test02.java

---
+++ Practice/Test02.java
@@ -0,0 +1,19 @@
+/*
+
+	小芳的妈妈每天给她2.5元钱，她都会存起来，但是，每当这一天是存钱的第5天
+	或者5的倍数的话，她都会花去6元钱，请问，经过多少天，小芳才可以存到100元钱。
+
+*/
+   
+	public class Test02{   
+		public static void main(String[] args){
+			int day = 0; double money = 0.0;
+		    while ( money < 100){ 
+				day++;
+		        money += 2.5;
+			    if ( day % 5 == 0){  
+				money -= 6;
+			 } 
+		   }System.out.println("小芳经过" + day + "可以存到" + money	+ "元");
+		}
+	}
