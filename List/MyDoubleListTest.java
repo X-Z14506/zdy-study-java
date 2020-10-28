9877df8bb813db5c1dbc3adffe404a871581ad0c
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-10-28 20:33:03 +0800

    Create MyDoubleListTest.java

---
+++ List/MyDoubleListTest.java
@@ -0,0 +1,22 @@
+
+
+public class MyDoubleListTest {
+
+
+    public static void main(String[] args) {
+        DoubleList doubleList = new DoubleList();
+        doubleList.addLast(4);
+        doubleList.addLast(4);
+        doubleList.addLast(4);
+        doubleList.addLast(4);
+        /*doubleList.addIndex(4,99);
+        doubleList.addFirst(88);*/
+        doubleList.display();
+       // doubleList.clear();
+        System.out.println("fdsafdsfa");
+
+        /*doubleList.removeAllKey2(4);
+        doubleList.display();*/
+
+    }
+}
