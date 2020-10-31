483436a1034268931d6b476febb9af8b35582d91
X-Z14506 <72679349+X-Z14506@users.noreply.github.com>
2020-10-31 17:21:30 +0800

    Create ExceptionTestDemo03.java

---
+++ Practice/Exception_Test/ExceptionTestDemo03.java
@@ -0,0 +1,28 @@
+
+public class ExceptionTestDemo03 {
+    public static void main(String[] args) {
+        try{
+            throw ExceptionMethod(true);
+        }catch(ArrayIndexOutOfBoundsException ex){
+            System.out.println("处理数组下标越界异常");
+        }catch(NullPointerException ex){
+            System.out.println("处理空指针异常");
+        }
+        finally {
+            System.out.println("无论怎样都得来我这一趟");
+        }
+    }
+    /*
+        抛出异常之前
+        处理空指针异常
+        无论怎样都得来我这一趟
+     */
+
+    private static void throw ExceptionMethod(boolean isThrow) {
+        System.out.println("抛出异常之前");
+        if(isThrow){
+            throw new NullPointerException();
+        }
+        System.out.println("抛出异常之后");
+    }
+}
